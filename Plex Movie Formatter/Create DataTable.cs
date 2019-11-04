f42c15957892cf38fd4ed33ed1a187ee4c1be479
MMachor@gmail.com <MMachor@gmail.com>
2019-11-03 20:13:32 -0600

    WIP

---
+++ Plex Movie Formatter/Create DataTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plex_Movie_Formatter
+{
+    class Create_DataTable
+    {
+        private Dictionary<String, Dictionary<String, String>> metadataCollected = new Dictionary<String, Dictionary<String, String>>();
+
+        public DataTable DataHandler(Dictionary<String, Dictionary<String, String>> metadataCollected)
+        {
+            this.metadataCollected = metadataCollected;
+            return CreateDatatable();
+        }
+
+        private DataTable CreateDatatable()
+        {
+            DataTable moviesInfo = new DataTable();
+
+            foreach (KeyValuePair<String, Dictionary<String, String>> movie in metadataCollected)
+            {
+                Dictionary<String, String> movieInformation = movie.Value;
+
+                foreach (String keyColumn in movieInformation.Keys)
+                {
+                    if (!moviesInfo.Columns.Contains(keyColumn))
+                    {
+                        if (keyColumn != "Transcode")
+                            moviesInfo.Columns.Add(keyColumn).DataType = typeof(String);
+                        else
+                            moviesInfo.Columns.Add(keyColumn).DataType = typeof(Boolean);
+                    }
+                }
+            }
+
+            return moviesInfo;
+        }
+    }
+}
